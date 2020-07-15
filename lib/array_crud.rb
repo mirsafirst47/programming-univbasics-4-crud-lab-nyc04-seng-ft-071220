32e34be3599c757dab76f3377915d72a5465884e
Samir Triande <habib.triande@gmail.com>
2020-07-15 16:23:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,14 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  #array = []
-  #element = "arrays!"
   array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array = []
-  element = "wow"
   array.unshift(element)
 end
 
